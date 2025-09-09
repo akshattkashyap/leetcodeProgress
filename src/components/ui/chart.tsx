28f7d4ca54d7a67cb543838c479a719ca9562c62
akshattkashyap <kashyapakshat1312@gmail.com>
2025-09-09 12:46:09 +0530

    removed redundant components

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import * as RechartsPrimitive from 'recharts';
-import {
-  NameType,
-  Payload,
-  ValueType,
-} from 'recharts/types/component/DefaultTooltipContent';
+// Removed unused imports from 'recharts/types/component/DefaultTooltipContent'
 
-import { cn } from '@/lib/utils';
+import { cn } from '../../lib/utils';
 
 // Format: { THEME_NAME: CSS_SELECTOR }
 const THEMES = { light: '', dark: '.dark' } as const;
