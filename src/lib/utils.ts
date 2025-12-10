d5d52b38e1f46fcaff9cd459546ad7027ff93f7d
akshattkashyap <kashyapakshat1312@gmail.com>
2025-12-11 01:53:10 +0530

    update totals

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const LEETCODE_TOTALS = {
-  easy: 910,
-  medium: 1944,
-  hard: 881
+  easy: 916,
+  medium: 1966,
+  hard: 891
 } as const;
